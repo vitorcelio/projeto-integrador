e44bfc36de29f8cb34feee38a208ed7457d572b1
HDeborah <deborah@ma.aluno.senai.br>
2021-07-07 16:43:16 -0300

    Adicionando método
    Método de adicionar software

--- Projeto-Integrador2/src/main/java/ControladorDao/EmpresaDao.java
+++ Projeto-Integrador2/src/main/java/ControladorDao/EmpresaDao.java
@@ -1,5 +1,23 @@
 package ControladorDao;
 
-public class EmpresaDao {
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+import conta.IdentificarSoftware;
+
+public class EmpresaDao {
+	public void adicionasoftware(IdentificarSoftware software) {
+		String sql = "insert into Identificar_Software " + "(software, cpf)" + "values (?, ?)";
+
+		try {
+			PreparedStatement stmt = connection.prepareStatement(sql);
+			stmt.setString(1, software.getSoftware());
+			stmt.setInt(2, software.getFuncionario().getCpf()); // Ligação com a chave estrangeira de Funcionario
+
+			stmt.execute();
+			stmt.close();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
